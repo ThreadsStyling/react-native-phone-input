184636ed19c978ed864884996a1536c39ecccf32
John Wright <johngeorge.wright@gmail.com>
2019-05-08 15:15:40 +0100

    feat: 🎸 add types

---
+++ lib/index.d.ts
@@ -0,0 +1,27 @@
+import React from 'react';
+import {TextInput, TextInputProps, StyleProp} from 'react-native';
+
+export interface PhoneInputProps {
+  initialCountry?: string;
+  allowZeroAfterCountryCode?: boolean;
+  disabled?: boolean;
+  value?: string | null;
+  style?: any;
+  flagStyle?: object;
+  textStyle?: object;
+  textProps?: TextInputProps;
+  textComponent?: React.FunctionComponent;
+  offset?: number;
+  pickerButtonColor?: string;
+  pickerBackgroundColor?: string;
+  pickerItemStyle?: object;
+  cancelText?: string;
+  confirmText?: string;
+  buttonTextStyle?: object;
+  onChangePhoneNumber?: (phoneNumber: string) => void;
+  onSelectCountry?: (iso2: string) => void;
+  onPressFlag?: () => void;
+  countriesList?: string[];
+}
+
+export default class PhoneInput extends React.Component<PhoneInputProps> {}
